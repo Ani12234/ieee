f784f1f41653878587eaa7c5b8fd01e18858c28f
Anirudh Kulkarni <anirudhkulkarni733@gmail.com>
2025-05-29 19:17:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://ieee-beta-five.vercel.app,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
